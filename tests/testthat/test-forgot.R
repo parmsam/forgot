96f7efcab68b03b3f2dbade862c5d41ed2bd6d0c
parmsam <parmartsam@gmail.com>
2023-05-17 14:18:46 -0400

    add tests to fix #5

--- tests/testthat/test-forgot.R
+++ tests/testthat/test-forgot.R
@@ -7,3 +7,30 @@ test_that("forgot() works", {
 
   expect_true(nrow(forgot_ex1)>1)
 })
+
+test_that("forgot2() works", {
+  forgot2_ex1 <- forgot::forgot2("stringr")
+  expect_s3_class(forgot2_ex1, "tbl_df")
+  expect_true(
+    all(
+      names(forgot2_ex1) %in% c("function_name", "title")
+    )
+  )
+})
+
+test_that("forgot_fx() works", {
+  forgot_fx_ex1 <- forgot::forgot_fx("dplyr", "count", "usage", print = F)
+  expect_s3_class(forgot_fx_ex1, "tbl_df")
+  expect_true(
+    names(forgot_fx_ex1) %in% c("usage")
+  )
+  forgot_fx_ex2 <- forgot::forgot_fx("dplyr", "count", print = F)
+  expect_s3_class(forgot_fx_ex2, "tbl_df")
+  expect_true(
+    all(
+      names(forgot2_ex1) %in% names(
+        forgot::forgot("dplyr", "count")
+      )
+    )
+  )
+})
